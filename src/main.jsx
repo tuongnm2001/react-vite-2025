5063bcd6cde766f5607d68b95beae94cf1faaa5e
Tuong Nguyen <tuongnm2001@gmail.com>
2025-03-27 15:28:35 +0700

    setup folder project

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import LoginPage from './pages/login.jsx';
+import RegisterPage from './pages/register.jsx';
+import UserPage from './pages/user.jsx';
+import ProductPage from './pages/product.jsx';
+import './styles/global.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+
+  {
+    path: "/register",
+    element: <RegisterPage />,
+  },
+
+
+  {
+    path: "/users",
+    element: <UserPage />,
+  },
+
+
+  {
+    path: "/products",
+    element: <ProductPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ src/pages/login.jsx
@@ -0,0 +1,7 @@
+const LoginPage = () => {
+    return (
+        <div>Login!@3</div>
+    );
+}
+
+export default LoginPage;
